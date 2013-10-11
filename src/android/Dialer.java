619ec648cee01eff2f257a5527df35920fa53815
primaaxa36 <prima.axa36@gmailcom>
2013-10-11 11:33:18 +0700

    new update

--- src/android/Dialer.java
+++ src/android/Dialer.java
@@ -1,30 +1,38 @@
 package com.prima.dialplugin;
 
+import org.apache.cordova.Config;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.UriResolver;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.net.Uri;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 
 public class Dialer extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException{
-		try{
-			if ("call".equals(action)) {
-				Uri number = Uri.parse("tel:" + args.getString(0));
+		if (action.equals("call")) {
+			String source = args.getString(0);
+
+			try{
+				Uri number = Uri.parse("tel:" + source);
 				Intent callIntent = new Intent(Intent.ACTION_CALL, number);
 				this.cordova.getActivity().startActivity(callIntent);
 				callbackContext.success();
 				return true;
+			} catch (Exception e) {
+				System.err.println("Exception: " + e.getMessage());
+				callbackContext.error(e.getMessage());
+				return false;
 			}
-			callbackContext.error("Invalid action");
-			return false;
-		} catch (Exception e) {
-			System.err.println("Exception: " + e.getMessage());
-			callbackContext.error(e.getMessage());
-			return false;
 		}
+
+		callbackContext.error("Invalid action");
+		return false;
 	}
 }
